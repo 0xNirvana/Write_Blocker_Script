5e031e53b6dce40f2e2cecb444401fd947378354
0xNirvana <nirvana.ctf@gmail.com>
2022-04-19 20:51:02 -0400

    Updated service_ops and code set up to work till stopping udisks2.service

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 #Python3
-import subprocess
+from subprocess import *
 import os
 import sys
 import shutil
@@ -15,20 +15,41 @@ Module to perform actions on a given service using systemctl
 Usage: service_ops(<Action(Eg: "start", "stop")>, <Service Name>)
 """
 def service_ops(action, service_name):
-    command = ["systemctl"]
-    command.append(action)
-    command.append(service_name)
-
+    base = ["systemctl"]
+    implementCommand = base + [action, service_name]
+    # implementCommand = implementCommand.append(action)
+    # implementCommand = implementCommand.append(service_name)
+    
     try:
-        subprocess.check_output(command)
-    except subprocess.CalledProcessError as cpe:
+        check_output(implementCommand)
+    except CalledProcessError as cpe:
         if (cpe.returncode == 5):
             print(f"ERROR: Service name {service_name} does not exist!\n")
         elif (cpe.returncode == 4):
             print(f"ERROR: Execute script with sudo privileges!\n")
     else:
-        print(f"{action} command on {service_name} completed!\n")
+        statusCheckCommand = base + ["status"]
+        statusCheckCommand = statusCheckCommand + [service_name]
+        proc = Popen(statusCheckCommand, stdout=PIPE, stderr=PIPE)
+        serviceStatus = proc.communicate()[0].decode("utf-8")
+        # print(serviceStatus)
+        if action == "stop":
+            stopCheck(service_name, serviceStatus)
+        if action == "start":
+            startCheck(service_name, serviceStatus)
 
+def startCheck(service_name, serviceStatus):
+    if "Active: active" in serviceStatus:
+        print("Service %s started." % (service_name))
+    else:
+        print("Unable to start %s service." % service_name)     
+
+def stopCheck(service_name, serviceStatus):
+    if "Active: inactive" in serviceStatus:
+        print("Service %s stopped." % (service_name))
+    else:
+        print("Unable to stop %s service." % service_name)
+        
 def implementPatch():
     try:
         shutil.copy(FORENSIC_READ_ONLY_FILE, RULES_DST_PATH)
@@ -38,9 +59,11 @@ def implementPatch():
 
     try:
         files = os.listdir(TOOLS_DIRECTORY)
-        if os.path.exists(TOOLS_DST_PATH):
-            for f in files:
-                shutil.copy2(os.path.join(TOOLS_DIRECTORY,f), TOOLS_DST_PATH)
+        if not os.path.exists(TOOLS_DST_PATH):
+            os.mkdir(TOOLS_DST_PATH)
+        for f in files:
+            shutil.copy2(os.path.join(TOOLS_DIRECTORY,f), TOOLS_DST_PATH)
+        
     except Exception as e:
         print (e)
         sys.exit()
@@ -53,8 +76,17 @@ if __name__ == "__main__":
         print("Run this script with sudo.")
         sys.exit()
 
-    patchCheck = input("Have you implemented the kernel patch? Enter y/n:")
-    if patchCheck == "y" or patchCheck == "yes" or patchCheck == "Y":
-        patchResult = implementPatch()
-        
-    service_ops("stop", "colord")
+    while True:
+        patchCheck = input("Have you implemented the kernel patch? Enter y/n:")
+        if patchCheck == "n" or patchCheck == "no" or patchCheck == "N":
+            implementPatch()
+            break
+        elif patchCheck == "y" or patchCheck == "yes" or patchCheck == "Y":
+            print("Awesome!")
+            break
+        else:
+            continue
+    
+    
+    service_ops("stop", "udisks2.service")
+    # service_ops("stop", "colord")
